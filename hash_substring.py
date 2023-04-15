719a6b1f85b669e9a5edf6a92f7a1c75aff851e8
221RDB167 <124889522+221RDB167@users.noreply.github.com>
2023-04-15 22:02:35 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -4,7 +4,7 @@ def read_input():
     # this function acquires input both from keyboard and file
     choice = input().rstrip()
     if choice == "F":
-        with open('06.txt', 'r') as f:
+        with open('/tests/'+'06', 'r') as f:
             pattern = f.readline().rstrip()
             text = f.readline().rstrip()
     elif choice == "I":
